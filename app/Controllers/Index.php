18cc394c4c2cdca0e2b04778861c1452fb0ccaa8
floof09 <fapflores@gmail.com>
2024-11-29 18:51:20 +0800

    fixed deactivate

--- app/Controllers/Index.php
+++ app/Controllers/Index.php
@@ -3,8 +3,10 @@
 // Set the path
 namespace App\Controllers;
 
-class Index extends BaseController{
-    public function index(){
+class Index extends BaseController
+{
+    public function index()
+    {
         $data['title'] = "My Products Management Module";
 
         return view('include\header', $data)
@@ -13,7 +15,8 @@ class Index extends BaseController{
             .view('include\footer');
     }
 
-    public function about(){
+    public function about()
+    {
         $data['title'] = "About Me";
 
         return view('include\header', $data)
@@ -22,50 +25,55 @@ class Index extends BaseController{
             .view('include\footer');
     }
 
-    public function showData($name="Cute", $age=19){
+    public function showData($name = "Cute", $age = 19)
+    {
         echo "Hello, $name.<br>";
         echo "You are $age years old.<br>";
     }
 
-    public function login(){
+    public function login()
+    {
         // Check if submit button is clicked
-        if($this->request->is('POST')){
-            
+        if ($this->request->is('POST')) {
             // Load model
             $usersmodel = model('Users_model');
+
             // Retrieve data from form
             $logindata = $this->request->getPost(['username', 'password']);
+
             // Set filter then query from tblusers
-            $user  = $usersmodel->where('username', $logindata['username'])
+            $user = $usersmodel
+                ->where('username', $logindata['username'])
                 ->where('password', $logindata['password'])
                 ->first();
-                if ($user['status'] === 'deactivated') {
-                    return redirect()->back()->with('error', 'Your account is deactivated. Please contact the administrator.');
-                }
-            if(!$user){ // If there is no user found with the credentials
-                // Code when user is not found
-            }else{
-                // Redirects to home page if log in was successful
+
+            if (!$user) {
+                // If no user is found with the credentials
+                return redirect()->to('')->with('error', 'Invalid username or password.');
+            } elseif ($user['status'] == 'deactivated') {
+                // If the user's account is deactivated
+                return redirect()->to('')->with('error', 'Your account is deactivated. Please contact the administrator.');
+            } else {
+                // Redirects to home page if login is successful
                 return redirect()->to('home');
             }
-            
         }
-
 
         $data['title'] = "Log In";
 
         return view('include\header', $data)
-            .view('include\navbar')
-            .view('login_view')
-            .view('include\footer');
+            . view('include\navbar')
+            . view('login_view')
+            . view('include\footer');
     }
 
-    public function dd($data){
+    public function dd($data)
+    {
         echo "<pre>";
         echo print_r($data);
         echo "</pre>";
         die();
     }
 }
-
 ?>
+    
